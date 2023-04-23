eb86f3e04581be37f6003ac628b3236703281b9a
narasimha <naninarasimha27@gmail.com>
2023-04-24 00:21:45 +0530

    abc

--- frontend/src/Components/Petlist.js
+++ frontend/src/Components/Petlist.js
@@ -6,6 +6,7 @@ import ListItemText from "@mui/material/ListItemText";
 import { Avatar, Button, ListItemAvatar } from "@mui/material";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import axios from '../axios'
+import UpdateModal from "./UpdateModal"
 
 
 import Modal from './../Components/Modal';
@@ -22,7 +23,14 @@ export default function PetList() {
   const [errorMessage, seterrorMessage] = React.useState("")
   const [loading, setLoading] = React.useState(false)
   const [open, setOpen] = React.useState(false);
+  const [open1, setOpen1] = React.useState(false)
+  const [pet, setPet] = React.useState(2)
   const handleOpen = () => setOpen(true);
+  const handleOpen1 = (ind) => {
+    console.log("ind", ind)
+    setPet(ind)
+    setOpen1(true)
+  }
   const [finalPayload, setFinalPayload] = React.useState({})
   const handleClose = () => {
     setOpen(false)
@@ -78,27 +86,50 @@ export default function PetList() {
 
   }
 
+  const handleUpdateSubmit = async (payload) => {
+    axios.put(`pet/`, { ...payload, id: pets[pet]._id })
+      .then(res => {
+        window.location.reload(false)
+      })
+      .catch(err => {
+        setLoading(false)
+        seterrorMessage("Failed to create")
+      })
+  }
+
   return (
     <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       <Button variant="contained" onClick={handleOpen} sx={{ margin: 2 }}>Add Your PET</Button>
       <List component="nav" aria-label="main mailbox folders">
-        {pets.map((pet) => (
-          <ListItemButton
-            onClick={(event) => handleListItemClick(event, pet._id)}
-            style={{ border: "1px solid black", margin: "10px" }}
-          >
-            <ListItemAvatar>
-              <Avatar
-                alt="Remy Sharp"
-                src="https://media.istockphoto.com/id/1188690130/photo/angry-border-collie-dog-expression-face-isolated-on-white-background.jpg?s=612x612&w=is&k=20&c=JBHeFybW8F1a1mQuF5Zxd8DT8n7aYwTvsOcgeGFkyrw="
-              />
-            </ListItemAvatar>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <div style={{ display: "flex" }}>Name: <ListItemText primary={pet.name} style={{ marginLeft: "10px" }} /></div>
-              <div style={{ display: "flex" }}> Breed: <ListItemText primary={pet.breed} style={{ marginLeft: "10px" }} /></div>
-              <div style={{ display: "flex" }}> Medical Condition: <ListItemText primary={pet.medicalCondition} style={{ marginLeft: "10px" }} /></div>
+        {pets.map((pet, ind) => (
+          <div style={{ border: "1px solid black", margin: "10px" }}>
+            <ListItemButton
+              onClick={(event) => handleListItemClick(event, pet._id)}
+
+            >
+              <ListItemAvatar>
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://media.istockphoto.com/id/1188690130/photo/angry-border-collie-dog-expression-face-isolated-on-white-background.jpg?s=612x612&w=is&k=20&c=JBHeFybW8F1a1mQuF5Zxd8DT8n7aYwTvsOcgeGFkyrw="
+                />
+              </ListItemAvatar>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ display: "flex" }}>Name: <ListItemText primary={pet.name} style={{ marginLeft: "10px" }} /></div>
+                <div style={{ display: "flex" }}> Breed: <ListItemText primary={pet.breed} style={{ marginLeft: "10px" }} /></div>
+                <div style={{ display: "flex" }}> Medical Condition: <ListItemText primary={pet.medicalCondition} style={{ marginLeft: "10px" }} /></div>
+              </div>
+
+            </ListItemButton>
+            <div>
+              <Button
+                variant="contained"
+                style={{ margin: '10px' }}
+                onClick={(event) => {
+                  event.stopPropagation()
+                  handleOpen1(ind)
+                }}>Update</Button>
             </div>
-          </ListItemButton>
+          </div>
         ))}
       </List>
       <Modal
@@ -110,6 +141,8 @@ export default function PetList() {
         loading={loading}
         setFinalPayload={setPayload}
       />
+      {console.log("pet", pet)}
+      <UpdateModal open={open1} setOpen={setOpen1} mainObj={pets[pet]} handleFinalSubmit={handleUpdateSubmit} schema={schema} />
     </Box>
   );
 }
---
+++ frontend/src/Components/UpdateModal.js
@@ -0,0 +1,157 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { FormControl, InputLabel, MenuItem, NativeSelect, Select, TextField } from '@mui/material';
+
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+import { InsertInvitation } from '@mui/icons-material';
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function TransitionsModal({ open, setOpen, mainObj, handleFinalSubmit, schema }) {
+  const [errorMessage, seterrorMessage] = React.useState("")
+  const [loading, setLoading] = React.useState(false)
+  let intialPaylod = {}
+  const [finalPayload, setFinalPayload] = React.useState(intialPaylod)
+  React.useEffect(() => {
+    intialPaylod = {}
+    schema.map(obj => {
+      intialPaylod[obj.name] = mainObj[obj.name]
+    })
+    setFinalPayload(intialPaylod)
+    console.log("inside useeffect")
+  }, [mainObj])
+
+
+  const handleClose = () => {
+    setOpen(false)
+    seterrorMessage("")
+  };
+
+  const handleChange = (key, value) => {
+    setFinalPayload({ ...finalPayload, [key]: value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true)
+    await handleFinalSubmit(finalPayload)
+    seterrorMessage("failed")
+    setLoading(false)
+  }
+
+  console.log("mainObj", mainObj)
+  return (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
+        backdrop: {
+          timeout: 500,
+        },
+      }}
+    >
+      <Fade in={open}>
+        <Box sx={style}>
+          <Typography id="transition-modal-title" variant="h6" component="h2">
+            Enter PET Details
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            {
+              schema.map(obj => {
+                console.log(obj.name, mainObj[obj.name])
+                if (obj.type === "select") {
+                  return (
+                    <FormControl fullWidth variant="standard">
+                      <InputLabel id={obj.name}
+                        variant="standard" htmlFor="uncontrolled-native"
+                      >{obj.DisplayName}</InputLabel>
+                      <Select
+                        labelId={obj.name}
+                        id={obj.name}
+                        value={finalPayload[obj.name]}
+                        inputProps={{
+                          name: obj.name,
+                          id: 'uncontrolled-native',
+                        }}
+                        label={obj.DisplayName}
+                        onChange={(event) => { handleChange(obj.name, event.target.value) }}
+
+                      >
+                        {obj.options.map(option => (
+                          <MenuItem value={option}>{option}</MenuItem >
+                        ))}
+                      </Select>
+                    </FormControl>
+                  )
+                }
+
+                if (obj.type === "date") {
+                  return (
+                    <>
+                      <label>{obj.DisplayName}</label>
+                      <DatePicker
+                        selected={finalPayload[obj.name]}
+                        onChange={(value) => {
+                          handleChange(obj.name, value.toISOString())
+                        }}
+                      />
+                    </>
+                  )
+                }
+
+                return (
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    variant="standard"
+                    id={obj.name}
+                    label={obj.DisplayName}
+                    name={obj.name}
+                    value={finalPayload[obj.name]}
+                    onChange={(event) => { handleChange(obj.name, event.target.value) }}
+
+                  />
+                )
+              }
+
+              )
+            }
+            <span>{errorMessage}</span>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              disabled={loading}
+            >
+              Create
+            </Button>
+          </Box>
+        </Box>
+      </Fade>
+    </Modal>
+  );
+}
